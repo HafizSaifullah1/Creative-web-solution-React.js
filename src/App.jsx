7e079282e89ae0a20e19f2b69ae0e17e319f5f8c
Hafiz Saifullah <hafizsaifullahkhan1@gmail.com>
2024-10-20 21:15:53 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,8 @@ import Home from './component/Home/Home'
 import { useNavigate } from 'react-router-dom'
 
 function App() {
-const navigate = useNavigate();
-const goToProfile = ()=>{
-  navigate ("/profile")
-     
-}
-  const [count, setCount] = useState(0)
+
+  
 
   return (
     <>
